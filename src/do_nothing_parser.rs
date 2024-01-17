ed6f7bd18448c3f8e05caf381b7ad5c2e47caf8e
XieZongChen <952937570@qq.com>
2024-01-17 09:39:10 +0800

    feat: 增加笔记

--- src/do_nothing_parser.rs
+++ src/do_nothing_parser.rs
@@ -1,5 +1,9 @@
 use nom::IResult;
 
+/**
+ * 一个什么也干不了的解析器
+ */
 pub fn do_nothing_parser(input: &str) -> IResult<&str, &str> {
+    // nom 规定返回一个元祖，元组第一个元素是剩下的未解析的输入流部分，第二个元素是解析出的内容。
     Ok((input, ""))
 }
